44418f32e46e763682abc9b1eca04f5699089799
Karolina <karolina.zielinska4@gmail.com>
2020-04-25 15:47:56 +0200

    Computed changes

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,8 @@
         <a @click="logMeOut()">Wyloguj</a>
     </div>
 
-
     <div v-else>
       <login-form @login="logMeIn($event)"></login-form>
-      <login-form @login="logMeIn($event)" button-label="Wejdź"></login-form>
-      <login-form @login="logMeIn($event)" button-label="Wleć"></login-form>
-      <login-form @login="logMeIn($event)" :button-label="Math.random() < 0.5 ? 'Etykieta A' : 'Etykieta B'"></login-form>
     </div>
   </div>
 </template>
